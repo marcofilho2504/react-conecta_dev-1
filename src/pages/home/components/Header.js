2dd00a752cf5f6e2fb3d45b3c4a58cce466cf736
marcofilho2504 <marcoaurelio.filho25@gmail.com>
2021-09-16 16:23:42 -0300

    Initial code

---
+++ src/pages/home/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Header() {
+    return(
+        <header className = "Header">
+        <div className = "toolbar">
+            <div>
+                <a href = "/">Conecta Dev - Alteração</a>
+            </div>
+
+            <div>
+                <button>Novo Post</button>
+                <span>img1</span>
+                <span>img2 - Alteração</span>
+            </div>
+        </div>
+   </header>
+    )
+}
+
+export default Header;
