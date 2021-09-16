b27fa344058b5b31f6e6715ad138f16b461a54c7
marco <marcoaurelio.filho25@gmail.com>
2021-09-16 17:03:02 -0300

    test commit

--- src/pages/home/components/Header.js
+++ src/pages/home/components/Header.js
@@ -11,7 +11,7 @@ function Header() {
             <div>
                 <button>Novo Post</button>
                 <span>img1</span>
-                <span>img2 - Alteração</span>
+                <span>img2</span>
             </div>
         </div>
    </header>
